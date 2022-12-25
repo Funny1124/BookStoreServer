c3f91ce2796a4969237aa157020e29a13b6f560c
Cyan <204267667@qq.com>
2022-12-25 13:37:19 +0800

    订单表增加收货人，电话，地址信息

--- src/main/java/com/guet_unknown/bookstoreserver/mvc/domain/OrderItem.java
+++ src/main/java/com/guet_unknown/bookstoreserver/mvc/domain/OrderItem.java
@@ -14,7 +14,7 @@ import java.util.Date;
  * (OrderItem)实体类
  *
  * @author cyan
- * @since 2022-12-23 17:10:54
+ * @since 2022-12-25 13:35:53
  */
 @Slf4j
 @Data
@@ -22,20 +22,48 @@ import java.util.Date;
 @NoArgsConstructor
 @ApiModel("(OrderItem)实体类")
 public class OrderItem implements Serializable {
-    private static final long serialVersionUID = -24385653510415863L;
+    private static final long serialVersionUID = 609429770879517396L;
     @ApiModelProperty(value = "")
     private Long orderId;
     @ApiModelProperty(value = "")
     private Long userId;
     @ApiModelProperty(value = "")
     private String cartItems;
-    @ApiModelProperty(value = "")
+    /**
+     * 总价
+     */
+    @ApiModelProperty(value = "总价")
     private Double orderTotalPrice;
     /**
      * 是否已支付
      */
     @ApiModelProperty(value = "是否已支付")
     private Integer orderPay;
+    /**
+     * 收货人
+     */
+    @ApiModelProperty(value = "收货人")
+    private String name;
+    /**
+     * 收货人电话
+     */
+    @ApiModelProperty(value = "收货人电话")
+    private String phone;
+    /**
+     * 收货地址
+     */
+    @ApiModelProperty(value = "收货地址")
+    private String address;
+    /**
+     * 0未签收，1已签收
+     */
+    @ApiModelProperty(value = "0未签收，1已签收")
+    private Integer receiveStatus;
+    /**
+     * 签收人
+     */
+    @ApiModelProperty(value = "签收人")
+    private String receiveName;
     @ApiModelProperty(value = "")
     private Date createTime;
     @ApiModelProperty(value = "")
