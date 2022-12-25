be985446491be2e8a168d045d4114e1d3049dd3d
Cyan <204267667@qq.com>
2022-12-25 23:19:06 +0800

    地址增加默认字段 is_default

--- src/main/java/com/guet_unknown/bookstoreserver/mvc/domain/Address.java
+++ src/main/java/com/guet_unknown/bookstoreserver/mvc/domain/Address.java
@@ -14,7 +14,7 @@ import java.util.Date;
  * (Address)实体类
  *
  * @author cyan
- * @since 2022-12-20 20:04:28
+ * @since 2022-12-25 23:14:57
  */
 @Slf4j
 @Data
@@ -22,7 +22,7 @@ import java.util.Date;
 @NoArgsConstructor
 @ApiModel("(Address)实体类")
 public class Address implements Serializable {
-    private static final long serialVersionUID = -13624976183304074L;
+    private static final long serialVersionUID = -51574392548681608L;
     @ApiModelProperty(value = "")
     private Long addressId;
     /**
@@ -40,15 +40,20 @@ public class Address implements Serializable {
     @ApiModelProperty(value = "省")
     private String addressProvince;
     /**
-     * 市
+     * 市区
      */
-    @ApiModelProperty(value = "市")
+    @ApiModelProperty(value = "市区")
     private String addressCity;
     /**
      * 详情
      */
     @ApiModelProperty(value = "详情")
     private String addressDetail;
+    /**
+     * 是否为默认，1为默认地址
+     */
+    @ApiModelProperty(value = "是否为默认，1为默认地址")
+    private Integer isDefault;
     @ApiModelProperty(value = "")
     private Date createTime;
     @ApiModelProperty(value = "")
